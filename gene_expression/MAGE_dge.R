c4341c5f71517d9497c8eaf3faad849c19634d3a
Etienne Boileau <boileau@uni-heidelberg.de>
2021-03-19 15:53:38 +0100

    FIX dge coefficients

--- gene_expression/MAGE_dge.R
+++ gene_expression/MAGE_dge.R
@@ -1,56 +1,38 @@
-#Switch to working directory
-setwd("/Volumes/prj/MAGE/Christoph/stringtie")
-#read stringtie gene expression data
-
-gene_counts <- read.csv("gene_count_matrix.csv",row.names="gene_id",as.is=T)
-colnames(gene_counts)<-gsub("_stringtieRef","",colnames(gene_counts))
 
 library(stringr)
 library(edgeR)
 
-meta<-readRDS("/Volumes/prj/MAGE/Christoph/meta.rds")
+# use reference data
+dataDir <- '/prj/MAGE/analysis/data/stringtie'
+MAGNet_data <- load(file.path(dataDir, 'MAGNet_data.RData'))
 
-dedup=read.table("/Volumes/prj/MAGE/Christoph/mage_dedup_metrics_final.txt",as.is=T,header=F)
-dedup=dedup[,c(1,3)]
-dedup[,1]=gsub(".metrics.txt-Unknown","",dedup[,1])
-colnames(dedup)=c("Run","DuplicationRate")
-meta=merge(meta,dedup,by.x=1,by.y=1)
-rownames(meta)<-meta[,1]
+meta <- as.data.frame(cell.attrs)
+# already filtered 
+gene_counts <- cts[as.logical(as.numeric(levels(gene.attrs$GeneFlag))[gene.attrs$GeneFlag]),
+                   as.logical(as.numeric(levels(meta$SampleFlag))[meta$SampleFlag])]
+# filter meta after filtering counts!
+meta <- meta[as.logical(as.numeric(levels(meta$SampleFlag))[meta$SampleFlag]),]
+# then drop unused levels
+meta$Etiology <- droplevels(meta$Etiology)
 
-ins=intersect(rownames(meta),colnames(gene_counts))
 
-meta <- meta[ins, ]
-gene_counts <- gene_counts[, ins]
+design <- model.matrix(~ meta$Etiology + meta$Race + meta$Sex + meta$Age + meta$DuplicationRate)
 
-#library(edgeR)
-y <- DGEList(counts=gene_counts)
-
-design <- model.matrix(~ meta$etiology + meta$race + meta$sex + meta$Age + meta$DuplicationRate)
-design = design[,-c(5,7,9)]
-
-keep <- filterByExpr(y, design=design[,2:4])
+y <- DGEList(counts=gene_counts, group=meta$Etiology)
+keep <- filterByExpr(y, group=y$samples$group)
 y <- y[keep, , keep.lib.sizes=FALSE]
-
-
 y <- calcNormFactors(y)
-
-
 y <- estimateGLMCommonDisp(y, design, verbose=TRUE)
 y <- estimateGLMTagwiseDisp(y, design)
 
-#Todo save edgeR y object
-
 fit <- glmQLFit(y,design,robust=TRUE)
 
-                                        #Collect different contrast comparisons.
-
 QlfList<-list();
+QlfList[["DCMvsNormal"]] <- glmQLFTest(fit, coef=2) # DCM vs Normal
+QlfList[["HCMvsNormal"]] <- glmQLFTest(fit, coef=3)  # HCM vs Normal
+QlfList[["DCMvsHCM"]]<- glmQLFTest(fit,contrast=c(0,1,-1,0,0,0,0)) # DCM vs HCM
 
-QlfList[["DCMvsNormal"]] <- glmQLFTest(fit,contrast=c(0,1,0,-1,0,0,0,0)) #DCM vs Normal
-QlfList[["DCMvsHCM"]] <- glmQLFTest(fit,contrast=c(0,1,-1,0,0,0,0,0)) #DCM vs HCM
-QlfList[["HCMvsNormal"]]<- glmQLFTest(fit,contrast=c(0,0,1,-1,0,0,0,0)) #HCM vs Normal
-
-                                        #add annotation
+# add annotations
 library(biomaRt)
 mart=useMart(biomart="ENSEMBL_MART_ENSEMBL", dataset="hsapiens_gene_ensembl", host="apr2019.archive.ensembl.org")
 resMArt=getBM(attributes=c("ensembl_gene_id","hgnc_symbol","description"),mart=mart)
@@ -58,32 +40,30 @@ resMArt=getBM(attributes=c("ensembl_gene_id","hgnc_symbol","description"),mart=m
 #also use code for diseases..
 #Open Targets database genetic_association_mendelian_somatic_mutation_combined.bed Nov 25 2020
 
-Disease=read.delim("/Volumes//prj/MAGE/database/gene2disease.txt",header=F,as.is=T)
+Disease=read.delim("/prj/MAGE/database/gene2disease.txt",header=F,as.is=T)
 colnames(Disease)=c("ID","GeneSymbol","Disease")
 tmp=tapply(Disease$Disease,Disease$ID,paste,collapse=",")
 Disease=data.frame(ID=names(tmp),Disease=tmp)
 
-
 #RBP - from Hentze paper
 library(openxlsx)
 
-rbps=readWorkbook("/Volumes//prj/MAGE/database/TableS1.xlsx",2)
+rbps=readWorkbook("/prj/MAGE/database/TableS1.xlsx",2)
 rbps=as.character(subset(rbps$ID,rbps$found_in_at_least_2_Hs_RIC_studies=="YES" | rbps$knownRBPs=="YES" | rbps$"3470_human_RBPs"=="YES"))
 rbps=data.frame(ID=rbps,RBP=rep(1,length(rbps)))
 
 #Some TF paper    
-tfs=read.delim("/Volumes//prj/MAGE/database/annotated_human_TFs_slim.txt",header=F,as.is=T)
+tfs=read.delim("/prj/MAGE/database/annotated_human_TFs_slim.txt",header=F,as.is=T)
 tfs=unique(tfs[,2])
 tfs=data.frame(ID=tfs,TF=rep(1,length(tfs)))
 
-#exit(0);
-library(openxlsx)
+
 wb <- createWorkbook()
 n<-1;
 
 for(k in names(QlfList))
 {
-    DfQlf=as.data.frame(topTags(QlfList[[k]],n=22360))
+    DfQlf=as.data.frame(topTags(QlfList[[k]],n=dim(QlfList[[k]])[1]))
 
     DfQlf=merge(data.frame(ID=rownames(DfQlf),DfQlf),resMArt,by.x=1,by.y=1,all.x=T)
     DfQlf=merge(DfQlf,Disease,by.x=1,by.y=1,all.x=T)
@@ -99,5 +79,5 @@ for(k in names(QlfList))
     n=n+1;
 }
 
-saveWorkbook(wb, "MAGE_DGE_Nov2020.xlsx", overwrite = TRUE)
-exit(0);
+# adjust output path
+saveWorkbook(wb, "~/MAGE_DGE_Nov2020.xlsx", overwrite = TRUE)
